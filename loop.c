f0ec8aac6f2d1e22dc9eccb00e63c72862ad2646
Much3m <kga1800@uos.ac.kr>
2022-01-23 16:55:46 +0900

    first commit

---
+++ loop.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main(void)
+{
+    /* printf("Hello, World\n");
+    printf("Hello, World\n");
+    printf("Hello, World\n");
+    printf("Hello, World\n");
+    printf("Hello, World\n");
+    printf("Hello, World\n");
+    printf("Hello, World\n");
+    printf("Hello, World\n");
+    printf("Hello, World\n");
+    printf("Hello, World\n"); */
+
+    // ++
+   /*  int a=10;
+    printf("a는 %d\n",a);
+    a++;
+    printf("a는 %d\n",a);
+    a++;
+    printf("a는 %d\n",a); */
+
+    /* int b =20;
+    printf("b는 %d\n",++b); // 문장에서 ++ 먼저 수행
+    printf("b는 %d\n",b++); // 문장을 수행하고 나서 다음줄로 넘어갈 때 ++
+    printf("b는 %d\n",b); //b를 출력.
+    return 0; */
+
+    /* int i=1;
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++);
+    printf("Hello, World %d\n",i++); */
+
+    //반복문 
+    //for, while do, while
+    //for(선언;조건;증감)
+    for(int i=1; i<=10; i++)
+    {
+        printf("Hello, World %d\n",i);
+    }
+
+}
